d5827051c288caebad7e96d4da83108d4b32d88a
soniabergamini <sounia96@outlook.it>
2023-04-14 17:18:57 +0200

    feat(js): add Ticket Info

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,50 +4,61 @@ document.getElementById('createTicket').addEventListener("click",
 
         console.warn("Interaction: Click on CreateTicket Button.");
 
-        // Richiesta dati: Variabile Nome e Cognome (yourname)
+        // Variabile Nome e Cognome (yourname)
         let yourname = document.getElementById('formNameSurname').value;
 
         // Verifica la validità dei dati nella Variabile Nome e Cognome (yourname)
-        if (yourname == null || yourname == "" || yourname.length < 2) {
+        if (yourname == null || yourname == "" || yourname.length < 4) {
 
             // In caso la variabile Nome e Cognome (yourname) contenga dati mancanti, nulli o non validi:
-            alert("Sembra che il nome inserito non sia valido. Riprova, il tuo viaggio ti attende!");
             console.error(`'Invalid data for variable yourname. User input: ${yourname}`);
+            document.getElementById('guideText').innerText = ("Il valore inserito per il nome non è valido. Riprova.");
+            document.getElementById('guideText').style.display = 'block';
 
         } else {
 
-            // In caso la variabile Nome e Cognome (yourname) contenga dati corretti, l'utente può procedere:
-            document.getElementById("title").innerText = `Benvenuto su ExpressTrain ${yourname}!`;
+            // In caso la variabile Nome e Cognome (yourname) contenga dati corretti:
             console.log(`User declared the name: ${yourname}. The value is valid.`);
 
-            // Richiesta dati: Variabile Età del Passeggero (age)
+            // Variabile Età del Passeggero (age)
             let age = document.getElementById('formAge').value;
             console.log(`User declared the age: ${age}. The value is valid.`);
 
-            // Richiesta dati: Variabile Chilometri (kilometres)
+            // Variabile Chilometri (kilometres)
             let kilometres = document.getElementById('formKm').value;
             
             // Verifica la validità dei dati nella Variabile Chilometri (kilometres)
-            if (kilometres == null || kilometres == "" || kilometres < 1 || isNaN(kilometres) == true) {
+            if (kilometres < 1) {
+
                 // In caso la variabile Chilometri (kilometres) contenga dati mancanti, nulli o non validi:
-                alert("Il valore inserito non è valido. Riprova, il tuo viaggio ti attende!");
                 console.error(`Invalid data for variable kilometres. User input: ${kilometres}`);
+                document.getElementById('guideText').innerText = ("Il valore inserito per la distanza in km non è corretto. Riprova.");
+                document.getElementById('guideText').style.display = 'block'
                 
             } else {
                 // In caso la variabile Chilometri (kilometres) contenga dati corretti, l'utente può procedere:
                 console.log(`User declared the kilometres: ${kilometres}. The value is valid.`);
 
+                // Dichiarazione Informazioni di viaggio
+                document.getElementById("title").innerText = `Benvenuto ${yourname}!`;
+                document.getElementById("finalText").innerText = "ExpressTrain ti augura buon viaggio";
+                document.getElementById("ticketDetails").innerText = "I dettagli del tuo viaggio:";
+                document.getElementById("ticketName").innerText = `${yourname}`;
+                document.getElementById("ticketAge").innerText = `${age}`;
+                document.getElementById("ticketKm").innerText = `${kilometres}`;
+                document.getElementById('ticketInfo').style.display = 'block'
+
                 // Biglietto: Calcolo Prezzo Base
                 let ticketPrice = 0.21 * kilometres;
 
                 // Biglietto: Calcolo Prezzo Condizionale
-                if (age == "under18") {
+                if (age == "Minorenne") {
 
                     // Biglietto: Calcolo Prezzo Definitivo con sconto 20% minorenni
                     ticketPrice *= 0.8;
                     console.log("Ticket for Under18 users. Discount applied.");
 
-                } else if (age == "over65") {
+                } else if (age == "Più di 65 Anni") {
 
                     // Biglietto: Calcolo Prezzo Definitivo con sconto 40% dedicato agli over 65 anni
                     ticketPrice *= 0.6;
@@ -61,6 +72,7 @@ document.getElementById('createTicket').addEventListener("click",
                 }
 
                 // Dichiarazione del Prezzo del biglietto per tutti gli utenti che hanno inserito dati validi
+                document.getElementById("ticketPrice").innerHTML = `€${ticketPrice.toFixed(2)}`; 
                 console.log(`Ticket successfully created. The price for ${yourname}'s Ticket, who wishes to travel ${kilometres}km, is: €${ticketPrice.toFixed(2)}.`);
 
                 // Visibilità dei dati nel form post-creazione Biglietto
@@ -70,5 +82,33 @@ document.getElementById('createTicket').addEventListener("click",
     }
 );
 
+// Click sul pulsante 'Cancella':
+document.getElementById('resetTicket').addEventListener("click",
+    function(){
 
+        // Rimozione testo di errore:
+        document.getElementById('guideText').style.display = 'none'
+
+        // Rimozione Dettagli di Viaggio
+        document.getElementById('ticketInfo').style.display = 'none'
+    }
+);
+
+// Click su input "fornNameSurname":
+document.getElementById('formNameSurname').addEventListener("click",
+    function () {
+
+        // Rimozione testo di errore:
+        document.getElementById('guideText').style.display = 'none'
+    }
+);
+
+// Click su input "formKm":
+document.getElementById('formKm').addEventListener("click",
+    function () {
+
+        // Rimozione testo di errore:
+        document.getElementById('guideText').style.display = 'none'
+    }
+);
 
